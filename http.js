96e627dc5ae8b1b42b964bf172066aeae7cb6652
Violette Pumkins <74327188+Violette-Pumkins@users.noreply.github.com>
2020-12-23 14:57:56 +0100

    puuuuuush

--- http.js
+++ http.js
@@ -218,17 +218,7 @@ function rechGet(url) {
 
 
 
-function creerCompte() {
-    var nomVal = document.getElementById('fname').value;
-    var prenomVal = document.getElementById('lname').value;
-    var Id = Math.random() * 100;
 
-    post = {
-        nom: nomVal,
-        prenom: prenomVal
-    }
-    addUser('http://srvapi/api/stagiaire', post)
-}
 
 function modifier(event) {
     console.log(event.getAttribute("data-id"));
@@ -261,6 +251,7 @@ function recherche() {
         get(`http://srvapi/api/stagiaires/prenom/${prenomReche}`);
     }
 }
+
 
 
 
@@ -301,8 +292,8 @@ function afficher(tableauObjet) {
 }
 
 function creerCompte() {
-    var nomVal = document.getElementById('fname').value;
-    var prenomVal = document.getElementById('lname').value;
+    var nomVal = document.getElementById('nom').value;
+    var prenomVal = document.getElementById('prenom').value;
     var Id = Math.random() * 100;
 
     post = {
@@ -312,72 +303,4 @@ function creerCompte() {
     addUser('http://srvapi/api/stagiaire', post)
 }
 
-function modifier(event) {
-    console.log(event.getAttribute("data-id"));
-    var modID = event.getAttribute("data-id");
-    console.log(modID);
-    var modPost = {
-        nom: "teklit",
-        prenom: "TEWOLDE"
-    }
-    updateUser(`http://srvapi/api/stagiaire/${modID}`, modPost);
-}
-
-function supprimer(event) {
-    console.log(event.getAttribute("data-id"));
-    var delID = event.getAttribute("data-id");
-    console.log(delID);
-
-    delUser(`http://srvapi/api/stagiaire/${delID}`);
-}
-// recherche
-function recherche() {
-    var nomReche = document.getElementById("nomrecherche").value;
-    var prenomReche = document.getElementById("prenomrecherche").value;
-    if (nomReche && nomReche.length < 2) {
-        //console.log(nomReche);
-        get(`http://srvapi/api/stagiaires/nom/${nomReche}`);
-    }
-    if (prenomReche && prenomReche.length < 2) {
-        // console.log(prenomReche);
-        get(`http://srvapi/api/stagiaires/prenom/${prenomReche}`);
-    }
-}
-
-
-
-
-
-/*************************************************************************************************************************************************************************** */
-
-function afficher(tableauObjet) {
-
-    var lists = document.getElementById('listes');
-    for (let i = 0; i < tableauObjet.length; i++) {
-
-        var newElement = document.createElement("li");
-        newElement.innerHTML = tableauObjet[i].nom + "  " + tableauObjet[i].prenom;
-        //console.log(tableauObjet[i].id);
-        lists.appendChild(newElement);
-        var modBtn = document.createElement("button");
-        modBtn.classList.add("btn");
-        modBtn.innerHTML = "Modifier";
-        modBtn.setAttribute("id", "modID")
-        modBtn.setAttribute("data-id", tableauObjet[i].id);
-        modBtn.setAttribute("onclick", "modifier(this)");
-        newElement.appendChild(modBtn);
-
-        var suppBtn = document.createElement("button");
-        suppBtn.classList.add("btn");
-        suppBtn.innerHTML = "Supprimer";
-        suppBtn.setAttribute("id", "supID")
-        suppBtn.setAttribute("data-id", tableauObjet[i].id);
-        suppBtn.setAttribute("onclick", "supprimer(this)");
-        newElement.appendChild(suppBtn);
-
-
-        // console.log(newElement);
-    }
-
-} 
-    
+  
